ed2f16e9e9892f05cbce34944ce97658ef7b8adc
AnnaDemchuk <annad@wizardsdev.com>
2026-02-05 21:00:53 +0100

    add lines

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 console.log('This is a test file.');
 console.log('test 1');
 console.log('test 2');
+console.log('test 2');
+console.log('test 2');
+console.log('test 2');
+console.log('test 2');
+console.log('test 2');
+console.log('test 2');
+console.log('test 2');

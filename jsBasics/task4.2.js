38eb24aa1736f629f12c71491c5ff212989b5357
AnnaDemchuk <34246748+AnnaDemchuk@users.noreply.github.com>
2026-02-15 00:22:07 +0100

    add files

---
+++ jsBasics/task4.2.js
@@ -0,0 +1,9 @@
+let length;
+let width;
+let area;
+
+length = 10;
+width = 2;
+
+area = length * width;
+console.log(`Length: ${length}, Width: ${width}, Area: ${area}`);
---
+++ jsBasics/task4.3.js
@@ -0,0 +1,24 @@
+let radius, height, cylinderVolume;
+
+radius = 4;
+height = 10;
+
+cylinderVolume = Math.PI * radius ** 2 * height;
+console.log(`cylinderVolume   : ${cylinderVolume}`);
+502.6548245743669;
+
+cylinderVolume = parseFloat(cylinderVolume.toFixed(2));
+console.log(`cylinderVolume toFixed: ${cylinderVolume}`);
+
+///-----------------------------
+import Decimal from 'decimal.js';
+
+let radiusDec = new Decimal(4);
+let heightDec = new Decimal(10);
+
+let cylinderVolumeDec = new Decimal(Math.PI).mul(radiusDec.pow(2)).mul(heightDec);
+console.log(`cylinderVolumeDec: ${cylinderVolumeDec}`); // 502.65482457436688
+
+cylinderVolumeDec = cylinderVolumeDec.toDecimalPlaces(2);
+
+console.log(`CylinderVolumeDec toDecimalPlaces: ${cylinderVolumeDec}`);

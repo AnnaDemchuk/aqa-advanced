f23830cfae97a2b86ec33f5c4605b30a21897c41
AnnaDemchuk <34246748+AnnaDemchuk@users.noreply.github.com>
2026-03-12 14:15:45 +0100

    added hw9 (#9)

---
+++ jsBasics/HW lesson 9/task 9.2.js
@@ -0,0 +1,8 @@
+const book = {
+  title: 'Harry Potter and the Sorcerer’s Stone',
+  author: 'J.K. Rowling',
+  year: 1997,
+};
+
+const { title, author, year } = book;
+console.log(`title: ${title}, author: ${author}, year: ${year}`);
---
+++ jsBasics/HW lesson 9/task 9.4.js
@@ -0,0 +1,6 @@
+const person = { firstName: 'Bob', lastName: 'Pit', age: 33 };
+person.email = 'bop.p@gmail.com.';
+
+delete person.age;
+
+console.log(person);

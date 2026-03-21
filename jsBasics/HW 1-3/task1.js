a0e009dfd81678248637366df1b6dac2de1f773b
AnnaDemchuk <34246748+AnnaDemchuk@users.noreply.github.com>
2026-03-21 16:32:17 +0100

    changes after prettier package

--- jsBasics/HW 1-3/task1.js
+++ jsBasics/HW 1-3/task1.js
@@ -4,14 +4,14 @@ let myBoolean;
 let myNull;
 let myUndefined;
 
-myString = 'Hello, World!';
+myString = "Hello, World!";
 myNumber = 42;
 myBoolean = true;
 myNull = null;
 myUndefined = undefined;
 
-console.log(typeof myString + ':' + `${myString}`); //string
-console.log(typeof myNumber + ':' + `${myNumber}`); //number
-console.log(typeof myBoolean + ':' + `${myBoolean}`); //boolean
-console.log(typeof myNull + ':' + `${myNull}`); //object, это историческая ошибка в JavaScript, которая сохраняется для совместимости. На самом деле, null не является объектом, а представляет собой примитивное значение, которое означает "нет значения" или "пустое значение".
-console.log(typeof myUndefined + ':' + `${myUndefined}`); //undefined
+console.log(typeof myString + ":" + `${myString}`); //string
+console.log(typeof myNumber + ":" + `${myNumber}`); //number
+console.log(typeof myBoolean + ":" + `${myBoolean}`); //boolean
+console.log(typeof myNull + ":" + `${myNull}`); //object, это историческая ошибка в JavaScript, которая сохраняется для совместимости. На самом деле, null не является объектом, а представляет собой примитивное значение, которое означает "нет значения" или "пустое значение".
+console.log(typeof myUndefined + ":" + `${myUndefined}`); //undefined
